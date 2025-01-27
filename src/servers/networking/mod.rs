bbcaf7f92485aca97f439a7c41f07037d67c9ab1
KubriksCodeTN <patrick.pastorelli@studenti.unitn.it>
2025-01-27 19:34:03 +0100

    beautify

--- src/servers/networking/mod.rs
+++ src/servers/networking/mod.rs
@@ -1,4 +1,5 @@
 use common::{networking::flooder::Flooder, ring_buffer::RingBuffer, slc_commands::ServerEvent};
+use crossbeam_channel::Sender;
 use log::{error, info, warn};
 use wg_2024::{
     network::{NodeId, SourceRoutingHeader},
@@ -18,24 +19,17 @@ impl Flooder for GenericServer {
     }
 
     #[inline]
-    fn get_neighbours(
-        &self,
-    ) -> impl ExactSizeIterator<
-        Item = (
-            &wg_2024::network::NodeId,
-            &crossbeam_channel::Sender<wg_2024::packet::Packet>,
-        ),
-    > {
+    fn get_neighbours(&self) -> impl ExactSizeIterator<Item = (&NodeId, &Sender<Packet>)> {
         self.packet_send.iter()
     }
 
-    fn has_seen_flood(&self, flood_id: (wg_2024::network::NodeId, u64)) -> bool {
+    fn has_seen_flood(&self, flood_id: (NodeId, u64)) -> bool {
         self.flood_history
             .get(&flood_id.0)
             .map_or(false, |r| r.contains(&flood_id.1))
     }
 
-    fn insert_flood(&mut self, flood_id: (wg_2024::network::NodeId, u64)) {
+    fn insert_flood(&mut self, flood_id: (NodeId, u64)) {
         self.flood_history
             .entry(flood_id.0)
             .or_insert(RingBuffer::with_capacity(64))
@@ -43,7 +37,7 @@ impl Flooder for GenericServer {
     }
 
     #[inline]
-    fn send_to_controller(&self, p: wg_2024::packet::Packet) {
+    fn send_to_controller(&self, p: Packet) {
         let _ = self.controller_send.send(ServerEvent::PacketSent(p));
     }
 }
